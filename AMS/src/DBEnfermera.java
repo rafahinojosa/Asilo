7a2cc04b7d63ea88de8badd4f078265f2a314c93
Afigue7 <angelfiguex@gmail.com>
2017-11-15 09:24:50 -0600

    Update DBEnfermera.java
    Se agrego el metodo ADD enfermera

--- AMS/src/DBEnfermera.java
+++ AMS/src/DBEnfermera.java
@@ -1,3 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.*;
+
+import com.mysql.jdbc.Statement;
 
 public class DBEnfermera {
 	
@@ -8,7 +15,40 @@ public class DBEnfermera {
 	}
 	
 	public void addEnfermera(Enfermera e) {
-		//Query para agregar con parametros de DB (usar gets para cada uno :( )
+	try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("Driver loaded!");
+		} 
+		catch (ClassNotFoundException e) 
+		{
+			throw new IllegalStateException("Cannot find the driver in the classpath!", e);
+		}
+
+		//insert into
+		try
+		{
+			// create our mysql database connection
+			String myDriver = "org.gjt.mm.mysql.Driver";
+			String myUrl = "jdbc:mysql://localhost:3306/asilo";
+			Class.forName(myDriver);
+			Connection conn = DriverManager.getConnection(myUrl, "root", "What an amazing password!");
+
+			// create the java statement
+			java.sql.Statement st = conn.createStatement();
+
+			// our SQL SELECT query. 
+			// if you only need a few columns, specify them by name instead of using "*"
+			st.executeUpdate (
+
+					"insert INTO enfermera (nombre, fechaNacimiento, direccion, sexo, zonaAsignada) "
+							+ "VALUES ('Dora Dominguez', '19880720', 'Privada X 193', 1, 2)");
+			st.close();
+		}
+		catch (Exception e)
+		{
+			System.err.println("Got an exception! ");
+			System.err.println(e.getMessage());
+		}
 	}
 	
 	public void deleteEnfermera(int id) {
